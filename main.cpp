a415520ea02705ff487498d3bedb780fc4d23f8c
Kushal Pathak <84308717+Kushal-Pathak@users.noreply.github.com>
2023-03-11 22:01:25 +0545

    texture and sprite
    Creating texture, loading texture, creating sprite, setting texture to sprite and displaying sprite.

--- main.cpp
+++ main.cpp
@@ -1,7 +1,14 @@
 #include<SFML/Graphics.hpp>
+#include <iostream>
 
 int main() {
-	sf::RenderWindow window(sf::VideoMode(500, 500), "My Window");
+	sf::RenderWindow window(sf::VideoMode(800, 800), "My Window");
+	sf::Texture texture;
+	sf::Sprite sprite;
+	if (!texture.loadFromFile("img.jpg")) {
+		std::cout << "Error loading image" << std::endl;
+	}
+	sprite.setTexture(texture);
 	while (window.isOpen()) {
 		sf::Event event;
 		while (window.pollEvent(event)) {
@@ -9,7 +16,8 @@ int main() {
 				window.close();
 			}
 		}
-		window.clear(sf::Color::Red);
+		window.clear(sf::Color::Black);
+		window.draw(sprite);
 		window.display();
 	}
 }
