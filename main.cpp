510c99db4ce53ec00b89d830c180671b4b9503aa
Kushal Pathak <84308717+Kushal-Pathak@users.noreply.github.com>
2023-03-11 22:35:26 +0545

    moving and getting position of an entity

--- main.cpp
+++ main.cpp
@@ -6,6 +6,9 @@ int main() {
 	
 	sf::CircleShape circle(50.0f);
 	circle.setPosition(100.f, 100.f);
+	circle.move(200.f, 200.f);
+	sf::Vector2f position = circle.getPosition();
+	std::cout << "x: " << position.x << " y: " << position.y << std::endl;
 	circle.setFillColor(sf::Color::Green);
 	circle.setOutlineThickness(10.0f);
 	circle.setOutlineColor(sf::Color::Red);
