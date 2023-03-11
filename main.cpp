1f0cdd26313a3e875785cd05535e22e43423b210
Kushal Pathak <84308717+Kushal-Pathak@users.noreply.github.com>
2023-03-11 21:32:55 +0545

    Learning to render window
    Clearing window with a color and displaying

--- main.cpp
+++ main.cpp
@@ -1,24 +1,15 @@
-#include <SFML/Graphics.hpp>
+#include<SFML/Graphics.hpp>
 
-int main()
-{
-    sf::RenderWindow window(sf::VideoMode(500, 500), "SFML works!");
-    sf::CircleShape shape(100.f);
-    shape.setFillColor(sf::Color::Green);
-
-    while (window.isOpen())
-    {
-        sf::Event event;
-        while (window.pollEvent(event))
-        {
-            if (event.type == sf::Event::Closed)
-                window.close();
-        }
-
-        window.clear();
-        window.draw(shape);
-        window.display();
-    }
-
-    return 0;
+int main() {
+	sf::RenderWindow window(sf::VideoMode(500, 500), "My Window");
+	while (window.isOpen()) {
+		sf::Event event;
+		while (window.pollEvent(event)) {
+			if (event.type == sf::Event::Closed) {
+				window.close();
+			}
+		}
+		window.clear(sf::Color::Red);
+		window.display();
+	}
 }
